62ae930227fed2af02a51f85812539afb6dd48dc
Coralie Pei <pei.coralie@gmail.com>
2022-07-18 10:41:46 +0200

    Changement de dépôt

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
 require_once './functions/autoLoad.php';
-require __DIR__ . 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 autoLoad("*.php");
 
 // Définir le fuseau horaire dans lequel le serveur se trouve
 date_default_timezone_set('Europe/Paris');
-
+$pdo = pdo();
+dump(pdo());
 /* Utiliser include ou require
 * include renvoie un avertissement simple en cas d'erreur
 * require renvoie une erreur fatale et arrête l'exécution du script
