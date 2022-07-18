461382ab1d5db08f4177ac7cd4076607ac7f17a9
Coralie Pei <pei.coralie@gmail.com>
2022-07-18 11:38:21 +0200

    Corrections mineures du formulaire

--- includes/frmLogin.php
+++ includes/frmLogin.php
@@ -1,15 +1,13 @@
 <form action="index.php?page=login" method="post">
-
         <label for="email">E-mail :</label>
         <input type="text" id="email" name="email" value="<?=$email?>" />
     </div>
     <div>
-        <label for="mdp1">Mot de passe :</label>
-        <input type="password" id="mdp1" name="mdp1" />
+        <label for="mdp">Mot de passe :</label>
+        <input type="password" id="mdp" name="mdp" />
     </div>
-
         <input type="reset" value="Effacer" />
-        <input type="submit" value="Envoyer" />
+        <input type="submit" value="Se connecter" />
     </div>
     <input type="hidden" name="frmLogin" />
 </form>
